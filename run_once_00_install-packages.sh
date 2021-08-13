54ddd3bc1d584e4c500ec0c644d8ff10219b5e54
Jeremy Hettenhouser <jhettenh@gmail.com>
2021-08-13 19:36:26 -0400

    fixed the bad merge

--- run_once_00_install-packages.sh
+++ run_once_00_install-packages.sh
@@ -26,7 +26,7 @@ for i in ${build_frameworks_homebrew[@]}; do
 done
 
 # TODO: Extract brew targets that require super user permissions and place them in bootstrap
-cli_applications_homebrew=(nginx p7zip vim fzf fd bat jq yq mackup jenv diff-so-fancy prettyping shfmt autopep8 clang-format gpg2 exa jenv pyenv nvm tldr)
+cli_applications_homebrew=(nginx p7zip vim fzf fd bat jq yq mackup jenv diff-so-fancy prettyping shfmt autopep8 clang-format gpg2 exa jenv pyenv nvm tldr ncdu htop)
 
 for i in ${cli_applications_homebrew[@]}; do
   brew install $i
@@ -36,35 +36,13 @@ echo "Installing misc plugins"
 brew install pyenv-virtualenv
 brew install pyenv-ccache
 
-  # TODO: Extract brew targets that require super user permissions and place them in bootstrap
-  cli_applications_homebrew=(nginx p7zip vim fzf fd bat jq yq mackup jenv diff-so-fancy prettyping shfmt autopep8 clang-format gpg2 exa jenv pyenv nvm tldr ncdu htop)
+if [[ $PLATFORM == "darwin"* ]]; then
+  echo "Installing all non-freeware packages with brew cask"
 
   gui_app_cask=(Vivaldi google-chrome docker visual-studio-code slack discord dropbox intellij-idea adoptopenjdk8 1password-cli)
-  gui_app_cask+=(iterm2 1password viscosity paw the-unarchiver macdown bartender)
+  gui_app_cask+=(iterm2 1password viscosity paw the-unarchiver macdown)
   for i in ${gui_app_cask[@]}; do
     brew install $i
   done
 
-elif [[ $OSTYPE == "linux-gnu"* ]]; then
-  echo "Installing linux specific apps"
-
-  if [[ $PLATFORM == "darwin"* ]]; then
-    echo "Installing all non-freeware packages with brew cask"
-
-    gui_app_cask=(Vivaldi google-chrome docker visual-studio-code slack discord dropbox intellij-idea adoptopenjdk8 1password-cli)
-    gui_app_cask+=(iterm2 1password viscosity paw the-unarchiver macdown)
-    for i in ${gui_app_cask[@]}; do
-      brew install $i
-    done
-
-  elif [[ $OSTYPE == "linux-gnu"* ]]; then
-    echo "Installing linux specific apps"
-
-    # https://stackoverflow.com/questions/38859145/detect-ubuntu-on-windows-vs-native-ubuntu-from-bash-script
-    if [[ $(grep -q Microsoft /proc/version) ]]; then
-      echo "WSL"
-    else
-      echo "native Linux"
-    fi
-  fi
 fi
