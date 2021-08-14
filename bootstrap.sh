dd16aa4068920cc1b804c7bc79b7db5c3b7d8114
Jeremy Hettenhouser <jhettenh@gmail.com>
2021-08-14 13:06:29 -0400

    Remove -y option that doesn't exist from opkg

--- bootstrap.sh
+++ bootstrap.sh
@@ -87,7 +87,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then
   elif [ -x "$(command -v opkg)" ]; then
     # TODO Prune way back installs on this branch; the types of things that run opkg don't need full stack dev tools
     sudo opkg update
-    sudo opkg install -y build-essential procps curl file git ca-certificates ldd zsh
+    sudo opkg install build-essential procps curl file git ca-certificates ldd zsh
   fi     
 
   #if [[ ${DISTRIB} = "Ubuntu"* ]]; then
