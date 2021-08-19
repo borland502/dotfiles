c8fa954c96cf304b2cfbe6a9b9a14540845149d4
Jeremy Hettenhouser <jhettenh@gmail.com>
2021-08-19 12:10:49 -0400

    Reconciled bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,68 +10,72 @@
 ###############################################################################
 ask_for_sudo() {
 
-    # Ask for the administrator password upfront.
+  # Ask for the administrator password upfront.
 
-    sudo -v &> /dev/null
+  sudo -v &> /dev/null
 
-    # Update existing `sudo` time stamp
-    # until this script has finished.
-    #
-    # https://gist.github.com/cowboy/3118588
+  # Update existing `sudo` time stamp
+  # until this script has finished.
+  #
+  # https://gist.github.com/cowboy/3118588
 
-    # Keep-alive: update existing `sudo` time stamp until script has finished.  This does not work with homebrew's few priviliged installs
-    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+  # Keep-alive: update existing `sudo` time stamp until script has finished.  This does not work with homebrew's few priviliged installs
+  while true; do
+    sudo -n true
+    sleep 60
+    kill -0 "$$" || exit
+  done 2> /dev/null &
 
-    echo "Password cached"
+  echo "Password cached"
 }
 
 ask_for_sudo
 
 ask() {
-    # https://djm.me/ask
-    local prompt default reply
+  # https://djm.me/ask
+  local prompt default reply
 
-    while true; do
+  while true; do
 
-        if [ "${2:-}" = "Y" ]; then
-            prompt="Y/n"
-            default=Y
-        elif [ "${2:-}" = "N" ]; then
-            prompt="y/N"
-            default=N
-        else
-            prompt="y/n"
-            default=
-        fi
+    if [ "${2:-}" = "Y" ]; then
+      prompt="Y/n"
+      default=Y
+    elif [ "${2:-}" = "N" ]; then
+      prompt="y/N"
+      default=N
+    else
+      prompt="y/n"
+      default=
+    fi
 
-        # Ask the question (not using "read -p" as it uses stderr not stdout)
-        echo -n "  [?] $1 [$prompt] "
+    # Ask the question (not using "read -p" as it uses stderr not stdout)
+    echo -n "  [?] $1 [$prompt] "
 
-        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
-        read reply </dev/tty
+    # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+    read reply < /dev/tty
 
-        # Default?
-        if [ -z "$reply" ]; then
-            reply=$default
-        fi
+    # Default?
+    if [ -z "$reply" ]; then
+      reply=$default
+    fi
 
-        # Check if the reply is valid
-        case "$reply" in
-            Y*|y*) return 0 ;;
-            N*|n*) return 1 ;;
-        esac
+    # Check if the reply is valid
+    case "$reply" in
+      Y* | y*) return 0 ;;
+      N* | n*) return 1 ;;
+    esac
 
-    done
+  done
 }
 
 # System dependant options that cannot be avoided for subsequent brew installs
 if [[ "$OSTYPE" == "linux"* ]]; then
-    DISTRIB=Other
+  DISTRIB=Other
 
-    # rare, but some distros do not have this file
-    if [[ -x "/etc/os-release" ]]; then
-      DISTRIB=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
-    fi
+  # rare, but some distros do not have this file
+  if [[ -x "/etc/os-release" ]]; then
+    DISTRIB=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+  fi
 
   # Options that apply to any linux system (WSL or otherwise)
 
@@ -79,7 +83,7 @@ if [[ "$OSTYPE" == "linux"* ]]; then
   if [ -x "$(command -v apt)" ]; then
     sudo apt-get update
     sudo apt-get -y install build-essential procps curl file git
-  elif [ -x "$(command -v yum)" ]; then
+  elif [[ -x "$(command -v yum)" ]]; then
     sudo yum update
     sudo yum -y groupinstall 'Development Tools'
     sudo yum -y install procps-ng curl file git
@@ -88,24 +92,24 @@ if [[ "$OSTYPE" == "linux"* ]]; then
     # TODO Prune way back installs on this branch; the types of things that run opkg don't need full stack dev tools
     sudo opkg update
     sudo opkg install build-essential procps curl file git ca-certificates ldd zsh
-  fi     
+  fi
 
   #if [[ ${DISTRIB} = "Ubuntu"* ]]; then
-    #if uname -a | grep -q '^Linux.*Microsoft'; then
-      # ubuntu via WSL Windows Subsystem for Linux
-    #else
-      # native ubuntu
-    #fi
+  #if uname -a | grep -q '^Linux.*Microsoft'; then
+  # ubuntu via WSL Windows Subsystem for Linux
+  #else
+  # native ubuntu
+  #fi
   #elif [[ ${DISTRIB} = "Debian"* ]]; then
-    # debian
+  # debian
   #fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS OSX prerequisite -- no harm if run again and it will also take care of git well enough for the initial stuff
   # -----------------------------------------------------------------------------
   # XCode -- From Slay (https://github.com/minamarkham/formation/blob/master/slay)
   # -----------------------------------------------------------------------------
-  if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
-    test -d "${xpath}" && test -x "${xpath}" ; then
+  if type xcode-select >&- && xpath=$(xcode-select --print-path) &&
+    test -d "${xpath}" && test -x "${xpath}"; then
     echo "Xcode already installed. Skipping."
   else
     step "Installing Xcode…"
@@ -130,14 +134,14 @@ fi
 
 # Most packages will be installed in the 00 script, but we need the rest of the files in order to proceed
 if ! [[ -x "$(command -v chezmoi)" ]]; then
-    brew install chezmoi
+  brew install chezmoi
 fi
 
 if ! [[ -d "$HOME/.local/share/chezmoi" ]]; then
-  # chezmoi init --apply --verbose --dry-run git@github.com:borland502/dotfiles.git 
+  # chezmoi init --apply --verbose --dry-run git@github.com:borland502/dotfiles.git
   chezmoi init git@github.com:borland502/dotfiles.git
   chezmoi diff
 fi
 
-  #TODO Pause script here for user go/nogo
-chezmoi apply  
+#TODO Pause script here for user go/nogo
+chezmoi apply
