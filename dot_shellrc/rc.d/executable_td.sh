9f37fe023fe2ecacb73fd37af7eac5ecac49b5ed
Jeremy Hettenhouser <jhettenh@gmail.com>
2021-10-04 10:42:06 -0400

    Added cli commandline syncing

--- dot_shellrc/rc.d/executable_td.sh
+++ dot_shellrc/rc.d/executable_td.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env sh
 
-TODO_DIR="$HOME/.todo"
-TODO_TXT="$TODO_DIR/todo.txt"
-
 td(){
-    gdrive download --path "$TODO_DIR" --force "$GDRIVE_TODO_TXT"
+    gdrive sync download "$GDRIVE_TODO_ROOT" "$HOME/.todo"
     todo.sh "$@" || exit
-    gdrive update --parent "$GDRIVE_TODO_ROOT" "$GDRIVE_TODO_TXT" "$TODO_TXT"
+    gdrive sync upload "$HOME/.todo" "$GDRIVE_TODO_ROOT"
 }
