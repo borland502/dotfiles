00adba35c5ddfbd9c600239865d9d2ba5c40bce1
Jeremy Hettenhouser <jhettenh@gmail.com>
2021-10-10 16:19:15 -0400

    Shell switching and ssh key generation

--- run_once_99_cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Set up additional frameworks and any tweaks/hacks/cleanup after the main work is done
-
-# Vundle
-if ! [[ -d "$HOME/.vim/bundle" ]]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  vim +PluginInstall +qall
-fi
