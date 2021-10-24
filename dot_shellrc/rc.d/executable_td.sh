dab3db029a060131653ce1a1cd7491e4f4f9a354
Jeremy Hettenhouser <jhettenh@gmail.com>
2021-10-24 11:31:34 -0400

    Fixed fzf and added todo.txt download for list command

--- dot_shellrc/rc.d/executable_td.sh
+++ dot_shellrc/rc.d/executable_td.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
-td() {
+tds() {
   gdrive sync download --keep-remote "$GDRIVE_TODO_ROOT" "$HOME/.todo" || exit
   todo.sh "$@" || exit
   gdrive sync upload --keep-local "$HOME/.todo" "$GDRIVE_TODO_ROOT"
+}
+
+td() {
+  gdrive sync download --keep-remote "$GDRIVE_TODO_ROOT" "$HOME/.todo" || exit
+  todo.sh "$@" || exit
 }
