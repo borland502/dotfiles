faf051fee2cb97dbe383ae6214aa7bffac69f8dd
Jeremy Hettenhouser <jhettenh@gmail.com[commit]>
2022-03-09 19:50:36 -0500

    Finally learning more about how to use zinit

--- scripts/shell/rc.d/executable_sqlv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# sqlcl function that makes use of the encrypted environment variables to log into the server
-
-sqlv() {
-    command sql -S "$USERNAME_LONG/$VLAB_DB_PASSWORD@jdbc:oracle:thin:@$VLAB_DB_HOST:1521:dte3" @"$1"
-}
