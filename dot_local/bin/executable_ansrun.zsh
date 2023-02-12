af13b5208c97afc02c69f46492570d1ef6d1d2f2
Jeremy Hettenhouser <jhettenh@gmail.com>
2023-02-12 14:27:26 -0500

    Adding brew caddy and ansible run scripts

--- dot_local/bin/executable_ansrun.zsh
+++ dot_local/bin/executable_ansrun.zsh
@@ -23,6 +23,7 @@ parser_definition() {
   param PLAYBOOK -p --playbook -- "Ansible Playbook to run"
   param TAG -t --tag -- "Tag to run within a playbook"
   param LIMIT -l --limit -- "Limit ansible to a particular host"
+  flag DEBUG -d --debug -- "Set debug '-vvvvv'"
   disp :usage -h --help
   disp VERSION --version
 }
@@ -40,4 +41,19 @@ if [[ -z "${PLAYBOOK}" ]]; then
   exit 1
 fi
 
-ansible-playbook "$HOME/scripts/ansible/playbooks/${PLAYBOOK}.yaml" -i "$HOME/scripts/ansible/inventory/hosts.ini" --vault-password-file ${HOME}/.ansible/vault_token
+# TODO: Make this parameter an array
+if [[ -n "${TAG}" ]]; then
+  _tag="--tag ${TAG}"
+fi
+
+# TODO: Make this parameter an array
+
+if [[ -n "${LIMIT}" ]]; then
+  _limit="--limit ${LIMIT}"
+fi
+
+if [[ -n "${DEBUG}" ]]; then
+  _debug="-vvvvv"
+fi
+
+ansible-playbook "$HOME/scripts/ansible/playbooks/${PLAYBOOK}.yaml" -i "$HOME/scripts/ansible/inventory/hosts.ini" --vault-password-file ${HOME}/.ansible/vault_token ${_tag} ${_limit} ${_debug}
