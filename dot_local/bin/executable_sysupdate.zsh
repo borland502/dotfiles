51c882250c7ac8c9c28629750aa0f70d69974ee7
Jeremy Hettenhouser <jhettenh@gmail.com>
2024-03-23 20:48:02 +0000

    Cleanup

--- dot_local/bin/executable_sysupdate.zsh
+++ dot_local/bin/executable_sysupdate.zsh
@@ -1,25 +1,37 @@
 #!/usr/bin/env zsh
 
 #Homebrew update and prune outdated cache
-brew update
-brew upgrade
-brew cleanup -s
+if [[ $(command -v brew) ]]; then
+  brew update
+  brew upgrade
+  brew cleanup -s
 
-#now diagnostic
-brew doctor
-brew missing
+  #now diagnostic
+  brew doctor
+  brew missing
+fi
 
-# NPM global update
-npm update -g
-npm install -g npm
+if [[ $(command -v npm) ]]; then
+  # NPM global update
+  npm update -g
+  npm install -g npm
+fi
 
-# Python3 updates (will error if you have nothing to update)
-pip3 install -U $(pip3 freeze | awk '{split($0, a, "=="); print a[1]}')
+if [[ $(command -v pip3) ]]; then
+  # Python3 updates (will error if you have nothing to update)
+  pip3 install -U $(pip3 freeze | awk '{split($0, a, "=="); print a[1]}')
+fi
 
-# misc updates
-tldr --update
+if [[ $(command -v tldr) ]]; then
+  # misc updates
+  tldr --update
+fi
 
 # OSX Specific updates
 if [[ -x "$(command -v softwareupdate)" ]]; then
   softwareupdate --all --install --force
+fi
+
+if [[ "$(command -v zi)" ]]; then
+  zi update --all
 fi
