63d9e07506c9376822d15b0158bb59a354971cd0
Jeremy Hettenhouser <jhettenh@gmail.com>
2024-05-24 15:22:53 -0400

    Added onchange script to mirror certain directories

--- .chezmoiscripts/run_onchange_00_copy_files.py
+++ .chezmoiscripts/run_onchange_00_copy_files.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
 import logging
-from xdg_base_dirs import xdg_state_home
+from pathlib import Path
+from xdg_base_dirs import xdg_state_home, xdg_data_home
+from plumbum.cmd import rsync
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename=xdg_state_home().joinpath("chezmoi.log"))
+
+sync_bin_cmd = rsync["-avzPh",f"{xdg_data_home()}/automation/dasbootstrap/bin/",f"{Path.home()}/.local/bin/"]
+logger.info(sync_bin_cmd())
+sync_lib_cmd = rsync["-avzPh",f"{xdg_data_home()}/automation/dasbootstrap/lib/",f"{Path.home()}/.local/lib/"]
+logger.info(sync_lib_cmd())
+sync_ansible_cmd = rsync["-avzPh", "--exclude",".git/"
+  f"{xdg_data_home()}/automation/dasbootstrap/ansible/",f"{Path.home()}/.ansible/"]
+logger.info(sync_ansible_cmd())
