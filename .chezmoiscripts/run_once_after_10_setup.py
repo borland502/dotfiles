85240e44af2fd93acdf4ca90edc99ec6eec3d215
Jeremy Hettenhouser <jhettenh@gmail.com>
2024-06-12 17:07:36 -0400

    Changed secrets paths for dasbootstrap

--- .chezmoiscripts/run_once_after_10_setup.py
+++ .chezmoiscripts/run_once_after_10_setup.py
@@ -8,29 +8,30 @@ from pykeepass import create_database, PyKeePass
 from pykeepass.group import Group
 from trapper_keeper.main import TrapperKeeper
 
-KEEPASS_DB_PATH: str = f"{Path.home()}/.local/share/keepass/secrets.kdbx"
-KEEPASS_DB_KEY: str = f"{Path.home()}/.config/keepass/key.txt"
-KEEPASS_DB_TOKEN: str = f"{Path.home()}/.local/state/keepass/keepass_token"
+KEEPASS_DB_PATH: str = f"{Path.home()}/.local/share/trapper_keeper/secrets.kdbx"
+KEEPASS_DB_KEY: str = f"{Path.home()}/.config/trapper_keeper/key.txt"
+KEEPASS_DB_TOKEN: str = f"{Path.home()}/.local/state/trapper_keeper/keepass_token"
 
 # adjust python shell path as by default the commands will be run non-interactively.  This is important for shell cmds
 os.environ["PATH"] = ":".join([f"{Path.home()}/.local/bin", f"{os.environ['PATH']}"])
 load_dotenv(dotenv_path=Path.home().joinpath(".env"), interpolate=True, override=True, encoding="utf-8")
 
+
 # Validate XDG variables and correct if necessary
 def validate_xdg_folders():
-  # TODO: Learn formatting
-  for xdg_func in [func for func in vars(xdg_base_dirs) if func.startswith("xdg")]:
-    path: Path = getattr(xdg_base_dirs, xdg_func)()
+    # TODO: Learn formatting
+    for xdg_func in [func for func in vars(xdg_base_dirs) if func.startswith("xdg")]:
+        path: Path = getattr(xdg_base_dirs, xdg_func)()
 
-    # some xdg vars are lists of paths
-    if isinstance(path, Path):
-      print(f"XDG variable {xdg_func.upper()} at {path} exists? {str(path.is_dir()).lower()}")
-    else:
-      print(f"XDG variable {xdg_func.upper()} with")
-      for resolved_path in [s_path for s_path in path]:
-        print(f"{resolved_path!s:<60} path exists? {str(resolved_path.is_dir()).lower()}")
-        if not resolved_path.is_dir():
-          resolved_path.mkdir(mode=0o700, parents=True)
+        # some xdg vars are lists of paths
+        if isinstance(path, Path):
+            print(f"XDG variable {xdg_func.upper()} at {path} exists? {str(path.is_dir()).lower()}")
+        else:
+            print(f"XDG variable {xdg_func.upper()} with")
+            for resolved_path in [s_path for s_path in path]:
+                print(f"{resolved_path!s:<60} path exists? {str(resolved_path.is_dir()).lower()}")
+                if not resolved_path.is_dir():
+                    resolved_path.mkdir(mode=0o700, parents=True)
 
 
 # Validate path
