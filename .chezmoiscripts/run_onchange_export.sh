d03c70e821d632e82a5e96c99a4ba41aaab9d65d
HDPC1_qnet <jeremy.hettenhouser@icf.com>
2024-11-10 16:57:10 -0500

    Trim env vars

--- .chezmoiscripts/run_onchange_export.sh
+++ .chezmoiscripts/run_onchange_export.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 # export chezmoi data to jinja compatible yaml in the ansible directory
-mkdir -p "${ANSIBLE_HOME}/inventory"
-chezmoi data --format=yaml > "${ANSIBLE_HOME}/inventory/group_vars/chezmoi_data.yaml"
+mkdir -p "${ANSIBLE_HOME}/inventory/group_vars"
+chezmoi data --format=yaml >"${ANSIBLE_HOME}/inventory/group_vars/chezmoi_data.yaml"
+chmod 600 "${ANSIBLE_HOME}/inventory/group_vars/chezmoi_data.yaml"
+
+sysupdate
