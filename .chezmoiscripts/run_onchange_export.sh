30d92144ca82fcc83078185f0880e1bb381c7b79
42245_icfcorp <>
2024-11-23 22:08:05 -0500

    Transitioning from age to keepassxc

--- .chezmoiscripts/run_onchange_export.sh
+++ .chezmoiscripts/run_onchange_export.sh
@@ -5,6 +5,12 @@ mkdir -p "${ANSIBLE_HOME}/inventory/group_vars"
 chezmoi data --format=yaml >"${ANSIBLE_HOME}/inventory/group_vars/chezmoi_data.yaml"
 chmod 600 "${ANSIBLE_HOME}/inventory/group_vars/chezmoi_data.yaml"
 
+chmod 600 "${XDG_CONFIG_HOME}/age/key.txt"
+
+if [[ $(command -v task) && "$(pwd)" == "${XDG_DATA_HOME}/automation/home-ops" ]]; then
+  task scripts:deploy
+fi
+
 if [[ $(command -v sysupdate) ]]; then
   sysupdate
 fi
