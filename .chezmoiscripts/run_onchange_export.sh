9410c060180e12dd56ca392e90c63528ba8e392d
borland502 <jhettenh@gmail.com>
2024-12-26 15:06:02 -0500

    Revert asdf back to pyenv and nvm.  Shift dot_env.tmpl into a list-wrapped dictionary for vars.

--- .chezmoiscripts/run_onchange_export.sh
+++ .chezmoiscripts/run_onchange_export.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # export chezmoi data to jinja compatible yaml in the ansible directory
 mkdir -p "${ANSIBLE_HOME}/inventory/group_vars"
@@ -7,10 +7,13 @@ chmod 600 "${ANSIBLE_HOME}/inventory/group_vars/chezmoi_data.yaml"
 
 chmod 600 "${XDG_CONFIG_HOME}/age/key.txt"
 
+_prev="$(pwd)"
+cd "${XDG_DATA_HOME}/automation/home-ops" || (echo "Could not change to ${XDG_DATA_HOME}/automation/home-ops" && exit 2)
 if [[ $(command -v task) && "$(pwd)" == "${XDG_DATA_HOME}/automation/home-ops" ]]; then
-  task scripts:deploy
+  task install
+  task sync
 fi
+cd "${_prev}" || (echo "Could not change to ${_prev}" && exit 2)
 
-if [[ $(command -v sysupdate) ]]; then
-  sysupdate
-fi
+# shellcheck disable=SC1090
+source ~/.zshrc
